672dfedf1ff99656379fb91856ff3ee042ff40a2
JereUR <jeremias.jdv@gmail.com>
2024-12-28 21:42:57 -0300

    feat: Errores de validations traducidos

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,25 +1,43 @@
 import { z } from "zod"
 
-const requiredString = z.string().trim().min(1, "Requerido")
+const requiredString = z.string().trim().min(1, "Este campo es requerido")
 
-const RoleEnum = z.enum(["ADMIN", "DEFAULT", "STAFF"])
-
-export type Role = z.infer<typeof RoleEnum>
-
-export const signUpSchema = z.object({
-  firstName: requiredString.regex(/^[a-zA-Z]+$/, "Solo se permite letras"),
-  lastName: requiredString.regex(/^[a-zA-Z]+$/, "Solo se permite letras"),
-  email: requiredString.email("Email inválido"),
-  role: RoleEnum,
-  gender: requiredString,
-  birthday: requiredString,
-  password: requiredString.min(8, "Debe tener como mínimo 8 caracteres"),
-})
+export const signUpSchema = z
+  .object({
+    firstName: requiredString.min(1, "El nombre es requerido"),
+    lastName: requiredString.min(1, "El apellido es requerido"),
+    email: requiredString.email("El correo electrónico es inválido"),
+    gender: z.enum(["Masculino", "Femenino", "Otros"], {
+      errorMap: () => ({ message: "Selecciona un género válido" }),
+    }),
+    birthday: z
+      .string({
+        required_error: "La fecha de nacimiento es requerida",
+        invalid_type_error: "Formato de fecha inválido",
+      })
+      .refine(
+        (date) => {
+          return !isNaN(Date.parse(date))
+        },
+        {
+          message: "Formato de fecha inválido",
+        },
+      ),
+    password: requiredString.min(
+      8,
+      "La contraseña debe tener al menos 8 caracteres",
+    ),
+    confirmPassword: requiredString,
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Las contraseñas no coinciden",
+    path: ["confirmPassword"],
+  })
 
 export type SignUpValues = z.infer<typeof signUpSchema>
 
 export const loginSchema = z.object({
-  email: requiredString.email("Email inválido"),
+  email: requiredString.email("El correo electrónico es inválido"),
   password: requiredString,
 })
 
