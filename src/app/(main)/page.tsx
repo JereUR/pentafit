165e0a9fe71b927b794ff1265320123dc336d94e
JereUR <jeremias.jdv@gmail.com>
2024-12-30 22:00:46 -0300

    feat: Navbar pasado a page de home

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
 import HomePageComponent from "./HomePageComponent"
+import NavBar from "./NavBar"
 
 export default function HomePage() {
 
   return (
     <>
+      <NavBar />
       <HomePageComponent />
     </>
   )
