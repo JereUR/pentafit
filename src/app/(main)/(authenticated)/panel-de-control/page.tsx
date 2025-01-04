70067978fab52d7369870909acd92c2d3492546e
JereUR <jeremias.jdv@gmail.com>
2025-01-04 20:45:59 -0300

    feat: Sintaxis fix

--- src/app/(main)/(authenticated)/panel-de-control/page.tsx
+++ src/app/(main)/(authenticated)/panel-de-control/page.tsx
@@ -3,7 +3,6 @@ import { Metadata } from "next"
 export const metadata: Metadata = {
   title: "Panel de control",
 }
-
 export default function DashboardPage() {
   return (
     <div className="p-6">
