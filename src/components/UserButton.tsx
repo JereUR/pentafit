4142e0eaf7d8b1f338064708f00ea01e4226043d
JereUR <jeremias.jdv@gmail.com>
2025-01-03 22:27:22 -0300

    feat: Boton de usuario agregado

---
+++ src/components/UserButton.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import Link from "next/link"
+import { useQueryClient } from "@tanstack/react-query"
+import { LogOutIcon, UserIcon } from "lucide-react"
+
+import { useSession } from "@/app/(main)/SessionProvider"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
+import { cn } from "@/lib/utils"
+import { logout } from "@/app/(auth)/action"
+import UserAvatar from "./UserAvatar"
+
+interface UserButtonProps {
+  className?: string
+}
+
+export default function UserButton({ className }: UserButtonProps) {
+  const { user } = useSession()
+
+  const queryClient = useQueryClient()
+
+  if (!user) return
+
+  return (
+    <DropdownMenu >
+      <DropdownMenuTrigger asChild>
+        <button className={cn("flex-none rounded-full", className)}>
+          <UserAvatar avatarUrl={user.avatarUrl} size={40} />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="z-[100]">
+        <DropdownMenuLabel>
+          Sesión iniciada como {user.firstName} {user.lastName}
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link href={`/usuarios/${user.id}`}>
+          <DropdownMenuItem className="cursor-pointer hover:bg-[hsl(var(--button-hover))] focus:bg-[hsl(var(--button-hover))]">
+            <UserIcon className="mr-2 size-4" />
+            Mi perfil
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem
+          onClick={() => {
+            queryClient.clear()
+            logout()
+          }}
+          className="cursor-pointer hover:bg-[hsl(var(--button-hover))] focus:bg-[hsl(var(--button-hover))]"
+        >
+          <LogOutIcon className="mr-2 size-4" />
+          Cerrar sesión
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
