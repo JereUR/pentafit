59c551f798b4d674acc9780032fd39b4acf85957
JereUR <jeremias.jdv@gmail.com>
2025-01-09 22:40:47 -0300

    feat: Opciones de membresia agregados

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,29 @@
-import { Prisma } from "@prisma/client"
+import { MembershipLevel, Prisma } from "@prisma/client"
 
 export const GENDER_OPTIONS = [
   { value: "Masculino", label: "Masculino" },
   { value: "Femenino", label: "Femenino" },
   { value: "Otros", label: "Otros" },
 ] as const
+
+export const MEMBERSHIP_OPTIONS = [
+  {
+    level: MembershipLevel.STANDARD,
+    price: 9.99,
+    description:
+      "Acceso básico a todas las prestaciones con hasta 1 establecimiento",
+  },
+  {
+    level: MembershipLevel.PREMIUM,
+    price: 19.99,
+    description: "Posibilidad de contar con hasta 3 establecimientos",
+  },
+  {
+    level: MembershipLevel.FULL,
+    price: 29.99,
+    description: "Posibilidad de contar con hasta 5 establecimientos",
+  },
+]
 
 export function getUserDataSelect() {
   return {
