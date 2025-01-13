95ddf5f9b128c9e888cb9c74e933e13da2432895
JereUR <jeremias.jdv@gmail.com>
2025-01-12 22:13:44 -0300

    feat: Componente AvatarInput pasado a componente global

--- src/app/(main)/(authenticated)/usuarios/[id]/EditUserForm.tsx
+++ src/app/(main)/(authenticated)/usuarios/[id]/EditUserForm.tsx
@@ -1,11 +1,9 @@
 "use client"
 
-import { useRef, useState } from "react"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Camera, X } from 'lucide-react'
-import Image, { StaticImageData } from "next/image"
-import Resizer from "react-image-file-resizer"
+import { X } from 'lucide-react'
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -23,7 +21,7 @@ import LoadingButton from "@/components/LoadingButton"
 import { updateUserProfileSchema, UpdateUserProfileValues } from "@/lib/validation"
 import { useUpdateProfileMutation } from "./mutations"
 import avatarPlaceholder from "@/assets/avatar-placeholder.png"
-import CropImageDialog from "@/components/CropImageDialog"
+import { AvatarInput } from "@/components/AvatarInput"
 
 const GENDER_OPTIONS = [
   { value: "Masculino", label: "Masculino" },
@@ -173,73 +171,6 @@ export function EditUserForm({ user, onClose }: EditUserFormProps) {
         </div>
       </form>
     </Form>
-  )
-}
-
-interface AvatarInputProps {
-  src: string | StaticImageData
-  onImageCropped: (blob: Blob | null) => void
-}
-
-function AvatarInput({ src, onImageCropped }: AvatarInputProps) {
-  const [imageToCrop, setImageToCrop] = useState<File>()
-
-  const fileInputRef = useRef<HTMLInputElement>(null)
-
-  function onImageSelected(image: File | undefined) {
-    if (!image) return
-
-    Resizer.imageFileResizer(
-      image,
-      1024,
-      1024,
-      "WEBP",
-      100,
-      0,
-      (uri) => setImageToCrop(uri as File),
-      "file",
-    )
-  }
-
-  return (
-    <>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={(e) => onImageSelected(e.target.files?.[0])}
-        ref={fileInputRef}
-        className="sr-only hidden"
-      />
-      <button
-        type="button"
-        onClick={() => fileInputRef.current?.click()}
-        className="group relative block"
-      >
-        <Image
-          src={src}
-          alt="Vista previa de avatar"
-          width={150}
-          height={150}
-          className="size-32 flex-none rounded-full object-cover"
-        />
-        <span className="absolute inset-0 m-auto flex size-12 items-center justify-center rounded-full bg-black bg-opacity-30 text-white transition-colors duration-200 group-hover:bg-opacity-25">
-          <Camera size={24} />
-        </span>
-      </button>
-      {imageToCrop && (
-        <CropImageDialog
-          src={URL.createObjectURL(imageToCrop)}
-          cropAspectRatio={1}
-          onCropped={onImageCropped}
-          onClose={() => {
-            setImageToCrop(undefined)
-            if (fileInputRef.current) {
-              fileInputRef.current.value = ""
-            }
-          }}
-        />
-      )}
-    </>
   )
 }
 
