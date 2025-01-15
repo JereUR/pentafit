9859852ab090ab344bed5bb15825ca94dc484830
JereUR <jeremias.jdv@gmail.com>
2025-01-15 04:39:54 -0300

    feat: Endpoint para obtener el detalle de establecimiento

--- src/app/api/facility/[id]/route.ts
+++ src/app/api/facility/[id]/route.ts
@@ -1,93 +1,83 @@
 import { NextRequest, NextResponse } from "next/server"
 import prisma from "@/lib/prisma"
+import { FacilityAllInfo } from "@/types/facility"
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: Promise<{ userId: string }> },
-) {
+  { params }: { params: Promise<{ id: string }> },
+): Promise<NextResponse> {
   try {
-    const id = (await params).userId
+    const id = (await params).id
 
-    const facilities = await prisma.user.findUnique({
+    const facility = await prisma.facility.findUnique({
       where: { id },
       select: {
-        facilities: {
+        id: true,
+        name: true,
+        description: true,
+        isActive: true,
+        isWorking: true,
+        logoUrl: true,
+        address: true,
+        email: true,
+        phone: true,
+        facebook: true,
+        instagram: true,
+        users: {
           select: {
-            facility: {
+            user: {
               select: {
-                id: true,
-                name: true,
-                description: true,
-                isActive: true,
-                isWorking: true,
-                logoUrl: true,
+                firstName: true,
+                lastName: true,
+                avatarUrl: true,
               },
             },
+          },
+        },
+        metadata: {
+          select: {
+            title: true,
+            logoWebUrl: true,
+            primaryColor: true,
+            secondaryColor: true,
+            thirdColor: true,
+            slogan: true,
           },
         },
       },
     })
 
-    if (!facilities || facilities.facilities.length === 0) {
+    if (!facility) {
       return NextResponse.json(
-        { error: "Sin establecimientos disponibles" },
+        { error: "Establecimiento no encontrado" },
         { status: 404 },
       )
     }
 
-    const simplifiedFacilities = facilities.facilities.map((f) => f.facility)
-
-    return NextResponse.json(simplifiedFacilities)
-  } catch (error) {
-    console.error("Error fetching facilities:", error)
-    return NextResponse.json(
-      { error: "Error al cargar los establecimientos" },
-      { status: 500 },
-    )
-  }
-}
-
-export async function PUT(
-  request: NextRequest,
-  { params }: { params: Promise<{ id: string }> },
-) {
-  const id = (await params).id
-  const body = await request.json()
-
-  try {
-    const facilityState = await prisma.facility.findUnique({
-      where: { id },
-      select: { isWorking: true },
-    })
-
-    if (!facilityState) {
-      return NextResponse.json(
-        { error: "Establecimiento no encontrado o no activo" },
-        { status: 404 },
-      )
+    const transformedFacility: FacilityAllInfo = {
+      name: facility.name,
+      description: facility.description,
+      email: facility.email,
+      phone: facility.phone,
+      address: facility.address,
+      instagram: facility.instagram,
+      facebook: facility.facebook,
+      isActive: facility.isActive,
+      isWorking: facility.isWorking,
+      logoUrl: facility.logoUrl,
+      metadata: facility.metadata,
+      users: facility.users.map(({ user }) => ({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        avatarUrl: user.avatarUrl,
+      })),
     }
 
-    await prisma.facility.updateMany({
-      where: { users: { some: { userId: body.userId } } },
-      data: { isWorking: false },
-    })
-
-    const updatedFacility = await prisma.facility.update({
-      where: { id },
-      data: { isWorking: !facilityState.isWorking },
-      select: {
-        id: true,
-        name: true,
-        logoUrl: true,
-        isWorking: true,
-      },
-    })
-
-    return NextResponse.json(updatedFacility)
+    return NextResponse.json(transformedFacility)
   } catch (error) {
-    console.error("Error updating facility:", error)
+    console.error("Error fetching facility:", error)
     return NextResponse.json(
-      { error: "Error al actualizar la instalación" },
+      { error: "Error al cargar el establecimiento" },
       { status: 500 },
     )
   }
