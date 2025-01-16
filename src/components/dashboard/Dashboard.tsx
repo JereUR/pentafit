af11cd489bde2ff5e5c6afc9dc385ae57eb30adc
JereUR <jeremias.jdv@gmail.com>
2025-01-15 23:09:51 -0300

    feat: Mostrar establecimientos activos y selector de establecimientos

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,27 +1,75 @@
 'use client'
 
-import { useWorkingFacility } from "@/contexts/WorkingFacilityContext"
-import noImage from '@/assets/no-image.png'
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
+import { Building2, Loader2 } from 'lucide-react'
 
-export default function Dashboard() {
-  const { workingFacility } = useWorkingFacility()
+import { useFacilities } from "@/hooks/useFacilities"
+import { Card, CardContent } from '../ui/card'
+import WorkingFacility from '../WorkingFacility'
+import FacilitiesSelector from './FacilitiesSelector'
+
+export default function Dashboard({ userId }: { userId: string }) {
+  const { facilities, isLoading: isLoadingFacilities, error: facilitiesError, setWorkingFacility, isUpdatingFacility, workingFacility } = useFacilities(userId)
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Establecimiento en area de trabajo:</p>
-      {workingFacility ?
-        <div>
-          <Avatar className="w-16 h-16 sm:w-10 sm:h-10">
-            <AvatarImage src={workingFacility.logoUrl || (noImage.src as string)} alt={workingFacility.name} />
-            <AvatarFallback>{workingFacility.name.slice(0, 2).toUpperCase()}</AvatarFallback>
-          </Avatar>
-          <p>Nombre: {workingFacility.name}</p>
-        </div>
-        :
-        <p>Sin establecimientos en area de trabajo </p>
-      }
+    <div className='flex justify-between px-8 w-full'>
+      <div className='hidden md:inline'>
+        <DashboardCard id={0} title='Establecimientos' icon={<Building2 />} value={facilities?.filter(f => f.isActive).length} loading={isLoadingFacilities} error={facilitiesError} />
+      </div>
+      <Card className="w-[300px] border-primary shadow-md">
+        <CardContent className="p-4">
+          <WorkingFacility isUpdatingFacility={isUpdatingFacility} />
+          <FacilitiesSelector facilities={facilities} setWorkingFacility={setWorkingFacility} workingFacility={workingFacility} isUpdatingFacility={isUpdatingFacility} />
+        </CardContent>
+      </Card>
     </div>
   )
 }
+
+interface DashboardContentProps {
+  id: number
+  title: string,
+  icon: React.ReactNode,
+  value: number | undefined,
+  loading: boolean
+  error: Error | null
+}
+
+export function DashboardCard({ id, title, icon, value, loading, error }: DashboardContentProps) {
+
+  if (error) {
+    return (
+      <Card
+        key={id}
+        className="w-[250px] h-[120px] border-primary shadow-md"
+      >
+        <CardContent className="flex flex-col justify-between h-full p-4">
+          <div className='flex items-center gap-2 text-primary'>
+            {icon}
+            <span className="md:text-xl font-bold">{title}</span>
+          </div>
+          <div className="flex justify-center items-end">
+            <span className="text-sm md:text-lg font-semibold text-foreground">Error al cargar datos</span>
+          </div>
+        </CardContent>
+      </Card>
+    )
+  }
+
+  return (
+    <Card
+      key={id}
+      className="w-[250px] h-[120px] border-primary shadow-md"
+    >
+      <CardContent className="flex flex-col justify-between h-full p-4">
+        <div className='flex items-center gap-2 text-primary'>
+          {icon}
+          <span className="md:text-xl font-bold">{title}</span>
+        </div>
+        {loading ? <Loader2 className='mx-auto animate-spin' /> : <div className="flex justify-center items-end">
+          <span className="text-2xl md:text-3xl font-semibold text-foreground">{value || 0}</span>
+        </div>}
+      </CardContent>
+    </Card>
+  )
+}
+
