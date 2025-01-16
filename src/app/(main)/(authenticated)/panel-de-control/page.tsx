fd902ab9db8b785885f284753e43d35b070c3139
JereUR <jeremias.jdv@gmail.com>
2025-01-15 23:05:17 -0300

    feat: Pasar al Dashboard userId

--- src/app/(main)/(authenticated)/panel-de-control/page.tsx
+++ src/app/(main)/(authenticated)/panel-de-control/page.tsx
@@ -1,15 +1,23 @@
 import { Metadata } from "next"
 
 import Dashboard from "@/components/dashboard/Dashboard"
+import { validateRequest } from "@/auth"
+import { redirect } from "next/navigation"
 
 export const metadata: Metadata = {
   title: "Panel de control",
 }
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const { user } = await validateRequest()
+
+  if (!user) {
+    redirect("/iniciar-sesion")
+  }
+
   return (
     <main className="flex container gap-5 p-5">
-      <Dashboard />
+      <Dashboard userId={user.id} />
     </main>
   )
 }
