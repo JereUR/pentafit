103bf710eafdbf4f3e0a09955c43c3b7128b5334
JereUR <jeremias.jdv@gmail.com>
2025-01-15 23:14:54 -0300

    fix: Mostrar solo como opciones aquellos establecimientos activos

--- src/components/dashboard/FacilitiesSelector.tsx
+++ src/components/dashboard/FacilitiesSelector.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 import noImage from '@/assets/no-image.png'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select'
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar'
@@ -26,6 +25,8 @@ export default function FacilitiesSelector({ facilities, setWorkingFacility, wor
     }
   }
 
+  const availableFacilities = facilities?.filter(f => f.id !== workingFacility?.id && f.isActive) || []
+
   return (
     <Select open={isSelectOpen} onValueChange={handleFacilityChange} value={workingFacility?.id} onOpenChange={setIsSelectOpen} disabled={isUpdatingFacility}>
       <SelectTrigger className="w-full mt-4">
@@ -34,22 +35,29 @@ export default function FacilitiesSelector({ facilities, setWorkingFacility, wor
         </SelectValue>
       </SelectTrigger>
       <SelectContent>
-        {facilities?.filter(f => f.id !== workingFacility?.id && f.isActive).map((facility) => (
-          <SelectItem
-            key={facility.id}
-            value={facility.id}
-            className='flex justify-start gap-2 p-2 cursor-pointer transition-colors duration-200 ease-in-out'
-          >
-            <div className='flex items-center gap-4 px-8'>
-              <Avatar className="w-8 h-8 md:w-10 md:h-10 ">
-                <AvatarImage src={facility.logoUrl || (noImage.src as string)} alt={facility.name} />
-                <AvatarFallback>{facility.name.slice(0, 2).toUpperCase()}</AvatarFallback>
-              </Avatar>
-              <span className='md:text-lg'>{facility.name}</span>
-            </div>
-          </SelectItem>
-        ))}
+        {availableFacilities.length > 0 ? (
+          availableFacilities.map((facility) => (
+            <SelectItem
+              key={facility.id}
+              value={facility.id}
+              className='flex justify-start gap-2 p-2 cursor-pointer transition-colors duration-200 ease-in-out'
+            >
+              <div className='flex items-center gap-4 px-8'>
+                <Avatar className="w-8 h-8 md:w-10 md:h-10 ">
+                  <AvatarImage src={facility.logoUrl || (noImage.src as string)} alt={facility.name} />
+                  <AvatarFallback>{facility.name.slice(0, 2).toUpperCase()}</AvatarFallback>
+                </Avatar>
+                <span className='md:text-lg'>{facility.name}</span>
+              </div>
+            </SelectItem>
+          ))
+        ) : (
+          <div className="p-4 text-xs md:text-sm text-center text-gray-500">
+            No existen otros establecimientos <span className="text-primary">activos.</span>
+          </div>
+        )}
       </SelectContent>
     </Select>
   )
 }
+
