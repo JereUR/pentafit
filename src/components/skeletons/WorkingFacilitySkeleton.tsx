bd25c29273d4d5ae0c808cc756f44a30f242a53f
JereUR <jeremias.jdv@gmail.com>
2025-01-22 03:17:32 -0300

    feat: Borde y padding agregado

--- src/components/skeletons/WorkingFacilitySkeleton.tsx
+++ src/components/skeletons/WorkingFacilitySkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function WorkingFacilitySkeleton() {
   return (
-    <div className='flex flex-col gap-4'>
+    <div className='flex flex-col gap-4 border rounded-md p-2 md:p-4'>
       <Skeleton className='h-5 w-5/6 mx-auto' />
       <div className='flex items-center gap-4 ml-1'>
         <Skeleton className="w-16 h-16 rounded-full" />
