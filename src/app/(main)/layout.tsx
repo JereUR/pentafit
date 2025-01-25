9fbc38b67d4253cd84c0685e882ce679c12c2320
JereUR <jeremias.jdv@gmail.com>
2025-01-24 22:26:47 -0300

    feat: SIntaxis fix

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,4 @@
-import {Analytics} from '@vercel/analytics/next'
+import { Analytics } from '@vercel/analytics/next'
 
 import { validateRequest } from "@/auth"
 import SessionProvider from "./SessionProvider"
