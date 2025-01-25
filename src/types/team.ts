ee7de522e13c1207518a6e6763f40ad081543d5e
JereUR <jeremias.jdv@gmail.com>
2025-01-24 22:21:47 -0300

    feat: Opciones para rol agregado

--- src/types/team.ts
+++ src/types/team.ts
@@ -11,6 +11,30 @@ export const columnsTeam: { key: keyof TeamData; label: string }[] = [
   { key: "facilities", label: "Establecimientos" },
 ]
 
+export interface RoleSelectType {
+  value: Role
+  description: string
+}
+
+export const rolesSelect: RoleSelectType[] = [
+  {
+    value: Role.SUPER_ADMIN,
+    description:
+      "Permite realizar cualquier acción dentro del area de trabajo, así como ver reportes y estadisticas varias.",
+  },
+  {
+    value: Role.ADMIN,
+    description:
+      "Permite realizar la mayoría de las acciones dentro del area de trabajo salvo el apartado de 'Establecimientos' y ver reportes y estadísticas varias.",
+  },
+  {
+    value: Role.STAFF,
+    description:
+      "Permite realizar solo tareas relacionadas con la administración de clientes, tales como actividades, rutinas, planes nutricionales, etc.",
+  },
+  { value: Role.CLIENT, description: "Cliente del establecimiento" },
+]
+
 export type TeamData = {
   id: string
   firstName: string
