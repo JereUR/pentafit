2abaafec95a58822b8af8c0f18689742f2c3bc30
JereUR <jeremias.jdv@gmail.com>
2025-01-24 22:20:07 -0300

    feat: Valores de rol actualizados

--- src/app/(main)/(authenticated)/layout.tsx
+++ src/app/(main)/(authenticated)/layout.tsx
@@ -2,7 +2,7 @@ import { validateRole } from "@/auth"
 import AuthenticatedLayout from "./authenticated-wrapper"
 
 export default async function AuthenticatedWrapper({ children }: { children: React.ReactNode }) {
-  const { role } = (await validateRole()) || { role: "DEFAULT" }
+  const { role } = (await validateRole()) || { role: "CLIENT" }
 
   return <AuthenticatedLayout userRole={role}>{children}</AuthenticatedLayout>
 }
