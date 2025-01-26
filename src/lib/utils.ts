aaa52c859016e035340c70f99d608e15e0e5a126
JereUR <jeremias.jdv@gmail.com>
2025-01-25 23:04:47 -0300

    feat: Funcion para formatear fecha con años

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { format, differenceInYears } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatBirthday = (birthday: string) => {
+  const date = new Date(birthday)
+  const formattedDate = format(date, "dd/MM/yyyy")
+  const age = differenceInYears(new Date(), date)
+  return `${formattedDate} (${age} años)`
 }
