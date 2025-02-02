61931e51c027f894a92c0c9fc670bbed5973cd4c
JereUR <jeremias.jdv@gmail.com>
2025-02-02 03:04:50 -0300

    feat: Skeleton para boton de exportar agregado

--- src/components/skeletons/TableSkeleton.tsx
+++ src/components/skeletons/TableSkeleton.tsx
@@ -6,12 +6,13 @@ export function TableSkeleton() {
       <div className="flex flex-col items-start gap-4 mb-4">
         <Skeleton className="h-8 w-48" />
         <div className="flex flex-col md:flex-row md:justify-between gap-2 w-full">
-          <div className="flex flex-col md:flex-row items-center gap-2">
-            <Skeleton className="h-10 w-80 md:w-64" />
-            <Skeleton className="h-10 w-80 md:w-24" />
+          <div className="flex items-center gap-2">
+            <Skeleton className="h-10 w-1/3 md:w-64" />
+            <Skeleton className="h-10 w-1/3 md:w-24" />
+            <Skeleton className="h-10 w-1/3 md:w-24" />
           </div>
           <div className="flex flex-col md:flex-row gap-2 items-center">
-            <Skeleton className="h-10 w-80 md:w-32" />
+            <Skeleton className="h-10 w-full md:w-32" />
           </div>
         </div>
       </div>
