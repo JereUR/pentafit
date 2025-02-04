01e2089c063386f731862986975cfe8721c0c8c7
JereUR <jeremias.jdv@gmail.com>
2025-02-03 21:39:54 -0300

    feat: Pagina de agenda agregada

---
+++ src/app/(main)/(authenticated)/agenda/page.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from "next"
+import { Suspense } from "react"
+
+import { validateRequest } from "@/auth"
+import WorkingFacility from "@/components/WorkingFacility"
+import { WorkingFacilitySkeleton } from "@/components/skeletons/WorkingFacilitySkeleton"
+import { TableSkeleton } from "@/components/skeletons/TableSkeleton"
+import DiariesDashboard from "@/components/diaries/DiariesDashboard"
+
+export const metadata: Metadata = {
+  title: "Agenda",
+}
+
+export default async function DiaryPage() {
+  const { user } = await validateRequest()
+
+  if (!user) return null
+
+  return (
+    <main className="flex container gap-5 md:p-5">
+      <div className="flex flex-col gap-5">
+        <section className="w-full flex justify-center md:justify-start">
+          <Suspense fallback={<WorkingFacilitySkeleton />}>
+            <WorkingFacility userId={user.id} />
+          </Suspense>
+        </section>
+        <section className="flex-1">
+          <Suspense fallback={<TableSkeleton />}>
+            <DiariesDashboard userId={user.id} />
+          </Suspense>
+        </section>
+      </div>
+    </main>
+  )
+}
