db6305f8501bf938403795da356502aba29f0055
JereUR <jeremias.jdv@gmail.com>
2025-02-07 23:21:56 -0300

    fix: Chequear facilities de usuario

--- src/app/api/facility/working-facility/route.ts
+++ src/app/api/facility/working-facility/route.ts
@@ -1,10 +1,24 @@
 import { NextResponse } from "next/server"
 import prisma from "@/lib/prisma"
+import { validateRequest } from "@/auth"
 
 export async function GET() {
   try {
+    const { user } = await validateRequest()
+
+    if (!user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+    }
+
     const workingFacility = await prisma.facility.findFirst({
-      where: { isWorking: true },
+      where: {
+        isWorking: true,
+        users: {
+          some: {
+            userId: user.id,
+          },
+        },
+      },
       select: {
         id: true,
         name: true,
