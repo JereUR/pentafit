0f0e851de5e5febc8504fa0664bfca0a2c38ca56
JereUR <jeremias.jdv@gmail.com>
2025-02-08 21:48:09 -0300

    feat: Funcion para crear notificacion global

---
+++ src/lib/notificationHelpers.ts
@@ -0,0 +1,34 @@
+import { type NotificationType, Prisma } from "@prisma/client"
+
+export async function createNotification(
+  tx: Prisma.TransactionClient,
+  issuerId: string,
+  facilityId: string,
+  type: NotificationType,
+  relatedId?: string,
+) {
+  const facility = await tx.facility.findUnique({
+    where: { id: facilityId },
+    include: { users: true },
+  })
+
+  if (!facility) throw new Error("Facility not found")
+
+  const notifications = facility.users.map((userFacility) => ({
+    recipientId: userFacility.userId,
+    issuerId,
+    facilityId,
+    type,
+    ...(relatedId && {
+      [type.toLowerCase().includes("activity")
+        ? "activityId"
+        : type.toLowerCase().includes("plan")
+          ? "planId"
+          : type.toLowerCase().includes("diary")
+            ? "diaryId"
+            : "userId"]: relatedId,
+    }),
+  }))
+
+  await tx.notification.createMany({ data: notifications })
+}
