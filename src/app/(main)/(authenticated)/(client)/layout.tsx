7e4a4ee6a9173f620e8a4e20a0996dda6156def8
JereUR <jeremias.jdv@gmail.com>
2025-03-23 23:17:49 -0300

    feat: Layoutt para usuario de tipo CLIENT agregado

---
+++ src/app/(main)/(authenticated)/(client)/layout.tsx
@@ -0,0 +1,23 @@
+import type React from "react"
+import { validateRequest, validateRole } from "@/auth"
+import { redirect } from "next/navigation"
+
+export default async function ClientLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const { user } = await validateRequest()
+  const roleData = await validateRole()
+
+  if (!user) {
+    redirect("/iniciar-sesion")
+  }
+
+  if (roleData?.role !== "CLIENT") {
+    redirect("/panel-de-control")
+  }
+
+  return <>{children}</>
+}
+
