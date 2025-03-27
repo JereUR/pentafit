fa3efc76afc995fc26f8e941cbb80efd9c4762b1
JereUR <jeremias.jdv@gmail.com>
2025-03-27 00:38:45 -0300

    fix: Link agregado

--- src/app/(auth)/iniciar-sesion/google/GoogleSignInButton.tsx
+++ src/app/(auth)/iniciar-sesion/google/GoogleSignInButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function GoogleSignInButton() {
   return (
@@ -7,13 +8,10 @@ export default function GoogleSignInButton() {
       className="bg-white text-black hover:bg-gray-100 hover:text-black"
       asChild
     >
-      <a
-        href="/iniciar-sesion/google"
-        className="flex w-full items-center gap-2"
-      >
+      <Link href="/iniciar-sesion/google" className="flex w-full items-center gap-2" legacyBehavior passHref>
         <GoogleIcon />
         Iniciar sesión con Google
-      </a>
+      </Link>
     </Button>
   )
 }
